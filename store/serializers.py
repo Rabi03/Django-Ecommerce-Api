b57467ab5130feeb966f0631d097853703a5cee3
Rabi Islam <islamrabi02@gmail.com>
2022-07-09 22:03:37 +0600

    Uploading project

---
+++ store/serializers.py
@@ -0,0 +1,98 @@
+from decimal import Decimal
+from rest_framework import serializers
+from .models import Cart, CartItem, Collection, Customer,Product,Review
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Collection
+        fields=['id','title','products_count']
+    products_count=serializers.IntegerField(read_only=True)
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Product
+        fields=['id','title','description','slug','inventory','unit_price','price_with_tax','collection']
+
+    price_with_tax=serializers.SerializerMethodField(method_name='calculate_tax')
+
+    def calculate_tax(self,product):
+        return product.unit_price*Decimal(1.1)
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Review
+        fields=['id','name','description','date']
+    
+    def create(self, validated_data):
+        product_id=self.context['product_id']
+        return Review.objects.create(product_id=product_id,**validated_data)
+
+
+class BasicProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Product
+        fields=['id','title','unit_price']
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=CartItem
+        fields=['id','product','quantity','total_price']
+    
+    total_price=serializers.SerializerMethodField(method_name='calculate_total_price')
+    product=BasicProductSerializer()
+
+    def calculate_total_price(self,item):
+        return item.quantity*item.product.unit_price
+        
+
+class CartSerializer(serializers.ModelSerializer):
+    id=serializers.UUIDField(read_only=True)
+    class Meta:
+        model=Cart
+        fields=['id','total_price','items']
+    items=CartItemSerializer(many=True,read_only=True)
+    total_price=serializers.SerializerMethodField(method_name='calculate_total_price')
+
+    def calculate_total_price(self,cart):
+        return sum([item.quantity*item.product.unit_price for item in cart.items.all()])
+
+class AddCartItemSerializer(serializers.ModelSerializer):
+    product_id=serializers.IntegerField()
+    class Meta:
+        model=CartItem
+        fields=['id','product_id','quantity']
+
+    def validate_product_id(self,value):
+        if not Product.objects.filter(pk=value).exists():
+            raise serializers.ValidationError("No Product with the given ID is not found")
+        return value
+
+
+    def save(self, **kwargs):
+        cart_id=self.context['cart_id']
+        product_id=self.validated_data['product_id']
+        quantity=self.validated_data['quantity']
+        try:
+            cart_item=CartItem.objects.get(cart_id=cart_id, product_id=product_id)
+            cart_item.quantity+=quantity
+            cart_item.save()
+            self.instance=cart_item
+        except CartItem.DoesNotExist:
+            self.instance=CartItem.objects.create(cart_id=cart_id,**self.validated_data)
+        
+        return self.instance
+
+
+class UpdateCartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=CartItem
+        fields=['quantity']
+
+class CustomerSerializer(serializers.ModelSerializer):
+    user_id =serializers.IntegerField(read_only=True)
+    class Meta:
+        model=Customer
+        fields=['id','user_id','phone','birth_date','membership']
+    
+    
---
+++ store/urls.py
@@ -0,0 +1,20 @@
+from cgitb import lookup
+from django.urls import path
+from . import views
+from rest_framework_nested import routers
+
+router=routers.DefaultRouter()
+router.register('products',views.ProductViewSet,basename='products')
+router.register('collections',views.CollectionViewSet)
+router.register('carts',views.CartViewSet,basename='carts')
+router.register('customers',views.CustomerViewSet)
+
+product_routers=routers.NestedDefaultRouter(router,"products",lookup='product')
+product_routers.register('reviews',views.ReviewViewSet,basename='product-reviews')
+
+cart_routes=routers.NestedDefaultRouter(router,'carts',lookup='cart')
+cart_routes.register('items',views.CartItemViewSet,basename='cart-items')
+
+
+# URLConf
+urlpatterns = router.urls+product_routers.urls+cart_routes.urls
